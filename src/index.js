fd5f0b533edfd7f1e3761d06f350b1f8611f0316
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-03-25 07:14:02 +0900

    勝者判定

--- src/index.js
+++ src/index.js
@@ -8,9 +8,29 @@ function Square(props){
     <button className="square"
       // onClickハンドラ内でthis.setStateを呼び出すことで、<button>がクリックされたら、常に再レンダーするようの伝えている
       onClick={props.onClick}>
-      { this.props.value }
+      { props.value }
     </button>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 class Board extends React.Component {
@@ -19,13 +39,20 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
   renderSquare(i) {
     return (
@@ -37,7 +64,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner:' + winner
+    } else {
+      status = 'Next Player:' + (this.state.xIsNext ? 'X' : '0')
+    }
 
     return (
       <div>
