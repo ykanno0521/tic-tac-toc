624f3610381da2daf496fda3cf6ec5ca7fbe7af4
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2021-03-26 07:01:46 +0900

    complete!!

--- src/index.js
+++ src/index.js
@@ -57,12 +57,13 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
+      stepNumber: 0,
     }
   }
 
   handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.lengh - 1];
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1 ];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -72,12 +73,20 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    })
+  }
   render() {
     const history = this.state.history
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -85,7 +94,7 @@ class Game extends React.Component {
         'Go to move' + move :
         'Go to game start';
       return (
-        <li>
+        <li key={ move }>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
